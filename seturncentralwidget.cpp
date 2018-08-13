356b4efa868b9aaea54ea266fc128d8edc9f8638
Gilhan Choi <ghchoi.choi@gmail.com>
2018-08-13 19:12:07 +0900

    centralWidget subwindow create

--- seturncentralwidget.cpp
+++ seturncentralwidget.cpp
@@ -1,11 +1,29 @@
 #include "seturncentralwidget.h"
 #include <QMdiArea>
+#include <QHBoxLayout>
 
 SeturnCentralWidget::SeturnCentralWidget(QWidget *parent) : QWidget(parent)
 {
+    createLayout();
+    creatSubWindow();
+}
+
+void SeturnCentralWidget::createLayout()
+{
     QHBoxLayout *layout = new QHBoxLayout();
     m_mdiArea = new QMdiArea();
+    QWidget *temp = new QWidget;
+    temp->setBaseSize(300,500);
     layout->addWidget(m_mdiArea);
+    layout->addWidget(temp);
 //    layout->setMargin(0);
     setLayout(layout);
 }
+
+void SeturnCentralWidget::creatSubWindow()
+{
+    QWidget *subwindow = new QWidget;
+    m_mdiArea->addSubWindow(subwindow);
+    subwindow->showMaximized();
+}
+
--- seturncentralwidget.h
+++ seturncentralwidget.h
@@ -2,7 +2,6 @@
 #define SETURNCENTRALWIDGET_H
 
 #include <QWidget>
-#include <QHBoxLayout>
 
 class QMdiArea;
 
@@ -11,8 +10,10 @@ class SeturnCentralWidget : public QWidget
     Q_OBJECT
 public:
     explicit SeturnCentralWidget(QWidget *parent = 0);
+    void creatSubWindow();
 
 private:
+    void createLayout();
     QMdiArea *m_mdiArea;
 
 signals:
