5f2ea9485cafca996e6ee87dd2ec55f88fd2ce2c
Gilhan Choi <ghchoi.choi@gmail.com>
2018-09-26 22:28:30 +0900

    plate option 추가....

--- subwindow.cpp
+++ subwindow.cpp
@@ -13,9 +13,16 @@ void SubWindow::initWindow()
     plateDataModel = new PlateDataModel;
     plateMainView = new PlateMainView;
     plateMainView->setModel(plateDataModel);
+
+    plateMainView->resizeColumnsToContents();
+    plateMainView->resizeRowsToContents();
+//    plateMainView->setColumnWidth(0,500);
+
     plateMainView->setSelectionBehavior(QAbstractItemView::SelectRows);
     plateMainView->setDragDropMode(QAbstractItemView::DragDrop);
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(plateMainView);
     setLayout(layout);
 }
+
+
