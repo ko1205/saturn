b0a005b21b973e6406042a746942905875e20031
choi gil han <ghchoi.choi@gmail.com>
2018-12-14 15:12:40 +0900

    DirSelector file move to saturn/src/

---
+++ test_dev/DirSelector_test/main.cpp
@@ -0,0 +1,11 @@
+#include "dirselector.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DirSelector w;
+    w.show();
+
+    return a.exec();
+}
