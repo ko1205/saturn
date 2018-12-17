3aa23eabb1ce7cc99b8537f1eff705d0a73002a9
choi gil han <ghchoi.choi@gmail.com>
2018-12-17 16:13:26 +0900

    centralwidget.cpp 불필요한 주석 정리

--- src/centralwidget.cpp
+++ src/centralwidget.cpp
@@ -7,19 +7,6 @@
 
 CentralWidget::CentralWidget(QWidget *parent) : QWidget(parent)
 {
-//    fileModel = new QFileSystemModel;
-//    fileModel->setRootPath("");
-//    fileModel->setFilter(QDir::AllDirs|QDir::NoDotAndDotDot|QDir::Drives);
-//    dirView = new QTreeView;
-
-
-//    dirView->setModel(fileModel);
-//    dirView->hideColumn(1);
-//    dirView->hideColumn(2);
-//    dirView->hideColumn(3);
-
-
-//    dirView->setStyleSheet("background-color:darkgray");
     dirSelector = new DirSelector;
     plateViewController = new PlateViewController;
 
@@ -31,8 +18,14 @@ CentralWidget::CentralWidget(QWidget *parent) : QWidget(parent)
     mainSplitter->setStretchFactor(0,0);
     mainSplitter->setStretchFactor(1,1);
 
+    /*
+     * setCollapsible 합수는 Splitter 화면이 완전히 접혀 들어갈지
+     * 여부를 판단 한다. (int index, bool collapse) index는 창순서, collapse는 true,false 값을 가진다.
+     */
+    mainSplitter->setCollapsible(0,false);
+    mainSplitter->setCollapsible(1,false);
+
     tab = new QTabWidget;
-//    tab->setContentsMargins(0,0,0,0);
 //    tab->setDocumentMode(true);
 //    tab->setStyleSheet("QTabWidget:tab-bar { left: 0em;} QTabBar:tab:selected{background-color:darkgray}");
 
@@ -56,50 +49,28 @@ CentralWidget::CentralWidget(QWidget *parent) : QWidget(parent)
     messageview->setText("test");
     messageview->append("dsdlkjfslkf");
 
-//   	fileModel = new QFileSystemModel;
-//    fileModel->setRootPath("/");
-//    dirView = new QTreeView;
 
-//    dirView->setModel(fileModel);
-
-//    QSplitter *mainSplitter = new QSplitter;
-
-//    mainSplitter->addWidget(dirView);
-//    mainSplitter->addWidget(new QTableView);
-
-
-    QVBoxLayout *layout = new QVBoxLayout;
     QVBoxLayout *viewlayout = new QVBoxLayout;
-
     viewlayout->setMargin(0);
     viewlayout->setContentsMargins(0,0,0,0);
 
-    QWidget *topWidget = new QWidget;
-
-    topWidget->setLayout(viewlayout);
-
-
-//    upButton = new QPushButton("UP");
     viewlayout->addWidget(tab);
-//    viewlayout->addWidget(upButton);
+
+    QWidget *topWidget = new QWidget;
+    topWidget->setLayout(viewlayout);
 
     QSplitter *splitter = new QSplitter(Qt::Vertical);
 
     splitter->addWidget(topWidget);
     splitter->addWidget(messageview);
 
+    QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(splitter);
-//    layout->addWidget(messageview);
+
     setLayout(layout);
 //    connect(dirView,SIGNAL(clicked(QModelIndex)),this,SLOT(printPath(QModelIndex)));
 }
 
-//void CentralWidget::debugConsol(QString message)
-//{
-//#ifdef QT_DEBUG
-//    qDebug(message.toLocal8Bit());
-//#endif
-//}
 
 void CentralWidget::printPath(QModelIndex index)
 {
