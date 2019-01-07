d552edcdfe6abe2f8ba0e0b423a543899f842fd8
choi gil han <ghchoi.choi@gmail.com>
2019-01-07 16:01:09 +0900

    Template View 에 type column 추가 정렬 방식 추가
    name순 정렬 후 type 별 정렬

--- src/templateview.cpp
+++ src/templateview.cpp
@@ -5,7 +5,7 @@ TemplateView::TemplateView(QWidget *parent)
 {
     templateModel = new QStandardItemModel;
 
-    templateModel->setHorizontalHeaderLabels(QStringList("TemplateView"));
+    templateModel->setHorizontalHeaderLabels(QStringList()<<"TemplateView" << "type");
 
     createIcon();
     createRootFolder();
@@ -36,10 +36,10 @@ void TemplateView::createIcon()
 void TemplateView::createRootFolder()
 {
     rootItem = new QStandardItem("/");
+    rootItem->setData(true,Qt::UserRole);
     rootItem->setIcon(folderIcon);
-    templateModel->appendRow(rootItem);
+    templateModel->appendRow(QList<QStandardItem*>()<<rootItem << new QStandardItem("folder"));
     rootItem->setEditable(false);
-
 }
 
 void TemplateView::contextMenuEvent(QContextMenuEvent *event)
@@ -50,19 +50,42 @@ void TemplateView::contextMenuEvent(QContextMenuEvent *event)
 void TemplateView::insertFolder()
 {
     QModelIndex index = currentIndex();
+    /*
+     * 선택된 Item이 폴더일 경우에만 children 생성
+     * 폴더일떄 와 파일일때 함수구조가 같으므로 나중에 하나의 함수로 통합정리
+     */
+    if(!index.data(Qt::UserRole).toBool())
+    {
+        index = index.parent();
+    }
     QStandardItem *newFolder = new QStandardItem("test_Folder");
+    newFolder->setData(true,Qt::UserRole);
     newFolder->setIcon(folderIcon);
-//    index == rootIndex() ||
-    templateModel->itemFromIndex(index)->appendRow(newFolder);
+    templateModel->itemFromIndex(index)->appendRow(QList<QStandardItem*>()<<newFolder << new QStandardItem("folder"));
+
+    /*
+     * 1차 nameing 순으로 정렬후 다시 타입 순으로 정렬 parent폴더 expand;
+     * 아래 내용도 Item 추가 하거나 naming 또는 타입 변경시에도 호출이 필요하므로 함수 또는
+     * SIGNAL로 변경 필요
+     */
+    rootItem->sortChildren(0);
+    rootItem->sortChildren(1,Qt::DescendingOrder);
     expand(index);
 }
 
 void TemplateView::newFileSlot()
 {
     QModelIndex index = currentIndex();
+    if(!index.data(Qt::UserRole).toBool())
+    {
+        index = index.parent();
+    }
     QStandardItem *newFile = new QStandardItem("newFilew");
+    newFile->setData(false,Qt::UserRole);
     newFile->setIcon(fileIcon);
-    templateModel->itemFromIndex(index)->appendRow(newFile);
+    templateModel->itemFromIndex(index)->appendRow(QList<QStandardItem*>()<<newFile << new QStandardItem("file"));
+    rootItem->sortChildren(0);
+    rootItem->sortChildren(1,Qt::DescendingOrder);
     expand(index);
 }
 
